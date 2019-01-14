c6feacb7059c28ae7c208bc865749b1973ac788e
CodyWalraven <codyw@assetpanda.com>
2019-01-13 20:33:37 -0600

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <p> Temp tag</p>
+      </Router>
+    )
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledH1 = styled.h1 `
+  overflow: hidden;
+  background-color: #f1f1f1;
+  padding: 20px 10px;
+`
+
+class Header extends React.Component {
+  render() {
+    return (
+      <StyledH1>Codys Website</StyledH1>
+    )
+  }
+}
+
+export default Header
