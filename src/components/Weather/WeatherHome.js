040f4d6e1beddc0e557416110617288d08b443ff
CodyWalraven <ipod22433@gmail.com>
2019-02-02 13:13:30 -0600

    first working weather

--- src/components/Weather/WeatherHome.js
+++ src/components/Weather/WeatherHome.js
@@ -1,5 +1,4 @@
 import React from "react"
-import {setState} from 'react'
 import axios from 'axios'
 
 export default class WeatherHome extends React.Component {
@@ -7,13 +6,17 @@ export default class WeatherHome extends React.Component {
     weather: "20f"
   }
 
+  getWeather = () => {
+    
+  }
+
   async componentDidMount() {
     const response = await axios.get(
-      "https://api.darksky.net/forecast/c888a9c0a5a934f804bc0dcfe32e2984/42.3601,-71.0589"
+      'https://api.openweathermap.org/data/2.5/weather?q=Frisco,usa&units=imperial&APPID=6d9a703f94f2006001b7094d1241d1e6'
     )
     console.log(response)
-    setState(() => ({
-      weather: response
+    this.setState(() => ({
+      weather: JSON.stringify(response)
     }))
   }
 
