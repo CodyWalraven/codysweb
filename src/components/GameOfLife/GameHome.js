c201c0a1a83256c4798a1d0581ee79754e3d646e
CodyWalraven <ipod22433@gmail.com>
2019-02-09 20:36:56 -0600

    add background image from local

--- src/components/GameOfLife/GameHome.js
+++ src/components/GameOfLife/GameHome.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Header from "./../Header"
 import styled from 'styled-components'
+import mountains from '../../assets/mountains.jpg'
 import GameOfLife from "./GameOfLife"
 
 const StyledDiv = styled.div`
-  background-image: url('https://images.unsplash.com/photo-1549571558-7ca00bbd1b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80');
+  background-image: url(${mountains});
   background-size: cover;
   height: 100vh;
 `
